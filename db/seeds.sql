6ca3e75caffd8900b3f1e355e64f204250a0918d
NoPlanet_NoParty <eclancy11@gmail.com>
2018-11-03 12:20:39 -0400

    adding

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bitchin Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Every Day Bro Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Fupa Burger", false);
